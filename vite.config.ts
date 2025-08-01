bdd8f98aa158e1d5b65f257d9761ebbcf7891d4a
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2025-08-01 22:37:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // 👈 this is important for Vercel/static builds
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: "/emi-calculator/",
 });
